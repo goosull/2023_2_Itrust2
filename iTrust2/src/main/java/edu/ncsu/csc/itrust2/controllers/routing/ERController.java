b86f5ccd7f63917d776f4646dc8472cd00e7b495
kwangsu Yu <yuokok813813@yonsei.ac.kr>
2023-11-22 10:31:07 +0000

    fix: Add import

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/routing/ERController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/routing/ERController.java
@@ -3,6 +3,7 @@ package edu.ncsu.csc.itrust2.controllers.routing;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.ncsu.csc.itrust2.models.enums.Role;
